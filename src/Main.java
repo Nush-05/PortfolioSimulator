bbf7e963d938a65033bde6836529aeb432c1a207
Nush-05 <Janushramesh@outlook.com>
2026-09-19 20:10:34 +0100

    Created multiple stock objects

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,25 @@ public class Main {
         System.out.println("Portfolio Simulator");
 
         Stock apple = new Stock("Apple", "AAPL", 220.00, 12.4);
+        Stock microsoft = new Stock("Microsoft", "MSFT", 500.00, 18.2);
+        Stock nvidia = new Stock("NVIDIA", "NVDA", 180.00, 25.6);
 
         System.out.println("Stock: " + apple.name);
         System.out.println("Ticker: " + apple.ticker);
         System.out.println("Price: £" + apple.price);
         System.out.println("Previous Year Return: " + apple.previousYearReturn + "%");
+
+        System.out.println();
+        System.out.println("Stock: " + microsoft.name);
+        System.out.println("Ticker: " + microsoft.ticker);
+        System.out.println("Price: £" + microsoft.price);
+        System.out.println("Previous Year Return: " + microsoft.previousYearReturn + "%");
+
+        System.out.println();
+        System.out.println("Stock: " + nvidia.name);
+        System.out.println("Ticker: " + nvidia.ticker);
+        System.out.println("Price: £" + nvidia.price);
+        System.out.println("Previous Year Return: " + nvidia.previousYearReturn + "%");
 
 
 
