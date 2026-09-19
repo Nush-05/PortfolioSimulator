5cd32ec9290c0a8f0c1c36c6ebfdf5f60b29cfd9
Nush-05 <Janushramesh@outlook.com>
2026-09-19 20:20:44 +0100

    Added Holding class

---
+++ src/Holding.java
@@ -0,0 +1,4 @@
+public class Holding {
+    Stock stock;
+    double quantity;
+}
--- src/Main.java
+++ src/Main.java
@@ -27,8 +27,16 @@ public class Main {
 
         portfolio.cash = 10000.00;
 
+        Holding appleHolding = new Holding();
+        appleHolding.stock = apple;
+        appleHolding.quantity = 2.5;
+
+        System.out.println("Holding: " + appleHolding.stock.name);
+        System.out.println("Shares: " + appleHolding.quantity);
+
         System.out.println();
         System.out.println("Portfolio Cash: £" + portfolio.cash);
+
 
     }
 }
