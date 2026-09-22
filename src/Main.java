7e6b25794625d928d6bd4f052c44a7a2a831d708
Nush-05 <Janushramesh@outlook.com>
2026-09-22 23:07:01 +0100

    Added interactive portfolio menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Portfolio Simulator");
 
         Stock apple = new Stock("Apple", "AAPL", 220.00, 12.4);
@@ -25,66 +28,116 @@ public class Main {
 
         Portfolio portfolio = new Portfolio(10000.00);
 
-        portfolio.buy(apple, 2.5);
-        portfolio.buy(apple, 1.5);
-        portfolio.buy(microsoft, 1.5);
-        portfolio.buy(nvidia, 3.0);
+        boolean running = true;
+        while(running) {
+            System.out.println();
+            System.out.println("Menu");
+            System.out.println("1. View Portfolio");
+            System.out.println("2. Buy Stock");
+            System.out.println("3. Sell Stock");
+            System.out.println("4. View Transactions");
+            System.out.println("5. Exit");
 
-        portfolio.sell(apple, 1.5);
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.println();
+                System.out.println("Number of Holdings: " + portfolio.holdings.size());
+
+                double totalHoldingsValue = 0;
+                for (int i = 0; i < portfolio.holdings.size(); i++) {
+                    Holding currentHolding = portfolio.holdings.get(i);
+
+                    double value = currentHolding.stock.price * currentHolding.quantity;
+
+                    totalHoldingsValue = totalHoldingsValue + value;
+
+                    System.out.println("Stock: " + currentHolding.stock.name);
+                    System.out.println("Shares: " + currentHolding.quantity);
+                    System.out.println("Value: £" + value);
+                }
+
+                double totalPortfolioValue = portfolio.cash + totalHoldingsValue;
+                double profitLoss = totalPortfolioValue - portfolio.startingCash;
+
+                System.out.println();
+                System.out.println("Total Holdings Value: £" + totalHoldingsValue);
+                System.out.println("Total Portfolio Value: £" + totalPortfolioValue);
+                System.out.println("Portfolio Cash: £" + portfolio.cash);
+                System.out.println("Profit/Loss: £" + profitLoss);
+            }
+            if (choice == 2) {
+                System.out.println();
+                System.out.println("Select a stock:");
+                System.out.println("1. Apple");
+                System.out.println("2. Microsoft");
+                System.out.println("3. NVIDIA");
+
+                System.out.print("Enter your choice: ");
+                int stockChoice = scanner.nextInt();
+                Stock selectedStock = null;
+
+                if (stockChoice == 1) {
+                    selectedStock = apple;
+                } else if (stockChoice == 2) {
+                    selectedStock = microsoft;
+                } else if (stockChoice == 3) {
+                    selectedStock = nvidia;
+                } else {
+                    System.out.println("Invalid stock choice.");
+                }
+
+                if (selectedStock != null) {
+                    System.out.print("Enter quantity: ");
+                    double quantity = scanner.nextDouble();
+
+                    portfolio.buy(selectedStock, quantity);
+                }
+
+            }
+            if (choice == 4) {
+                System.out.println("Transaction History:");
+
+                double totalBought = 0;
+                double totalSold = 0;
+                for (Transaction transaction : portfolio.transactions) {
+                    double transactionValue = transaction.quantity * transaction.price;
+
+                    if (transaction.type.equals("BUY")) {
+                        totalBought = totalBought + transactionValue;
+                    }
+
+                    if (transaction.type.equals("SELL")) {
+                        totalSold = totalSold + transactionValue;
+                    }
+
+                    System.out.println(transaction.type + " "
+                            + transaction.stock.name + " "
+                            + transaction.quantity + " shares at £"
+                            + transaction.price);
+                }
+
+                System.out.println();
+                System.out.println("Total Bought: £" + totalBought);
+                System.out.println("Total Sold: £" + totalSold);
+            }
+            if (choice == 5) {
+                System.out.println("Goodbye!");
+                running = false;
+            }
+        }
+
+        //portfolio.buy(apple, 2.5);
+        //portfolio.buy(apple, 1.5);
+        //portfolio.buy(microsoft, 1.5);
+        //portfolio.buy(nvidia, 3.0);
+
+        //portfolio.sell(apple, 1.5);
 
         apple.updatePrice(240.00);
 
-        System.out.println();
-        System.out.println("Number of Holdings: " + portfolio.holdings.size());
 
-        double totalHoldingsValue = 0;
-        for (int i=0; i< portfolio.holdings.size(); i++){
-            Holding currentHolding = portfolio.holdings.get(i);
-
-            double value = currentHolding.stock.price * currentHolding.quantity;
-
-            totalHoldingsValue = totalHoldingsValue + value;
-
-            System.out.println("Stock: " + currentHolding.stock.name);
-            System.out.println("Shares: " + currentHolding.quantity);
-            System.out.println("Value: £" + value);
-        }
-
-        double totalPortfolioValue = portfolio.cash + totalHoldingsValue;
-        double profitLoss = totalPortfolioValue - portfolio.startingCash;
-
-        System.out.println();
-        System.out.println("Total Holdings Value: £" + totalHoldingsValue);
-        System.out.println("Total Portfolio Value: £" + totalPortfolioValue);
-        System.out.println("Portfolio Cash: £" + portfolio.cash);
-        System.out.println("Profit/Loss: £" + profitLoss);
-        System.out.println();
-
-        System.out.println();
-        System.out.println("Transaction History:");
-
-        double totalBought = 0;
-        double totalSold = 0;
-        for (Transaction transaction : portfolio.transactions) {
-            double transactionValue = transaction.quantity * transaction.price;
-
-            if (transaction.type.equals("BUY")){
-                totalBought = totalBought + transactionValue;
-            }
-
-            if (transaction.type.equals("SELL")){
-                totalSold = totalSold + transactionValue;
-            }
-
-            System.out.println(transaction.type + " "
-                    + transaction.stock.name + " "
-                    + transaction.quantity + " shares at £"
-                    + transaction.price);
-        }
-
-        System.out.println();
-        System.out.println("Total Bought: £" + totalBought);
-        System.out.println("Total Sold: £" + totalSold);
 
 
 
